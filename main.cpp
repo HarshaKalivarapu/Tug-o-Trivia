74401a6e0e4b56b1aca53eb40a0b0f701b5ed082
HarshaKalivarapu <harshavk27@gmail.com>
2024-12-01 20:05:22 -0500

    fixed spelling of answer

--- main.cpp
+++ main.cpp
@@ -489,10 +489,10 @@ void statsMenu(class Stats *Player1, class Stats *Player2){
 
     //lists player 1's stats
     LCD.WriteAt("Player 1:", 0, 50);
-    smallFont.display("Correct Answer:", 0xffffff, 2, 73);
+    smallFont.display("Correct Answers:", 0xffffff, 2, 73);
     LCD.WriteAt(Player1->cAnswers, 93, 70);
 
-    smallFont.display("Incorrect Answer:", 0xffffff, 1, 93);
+    smallFont.display("Incorrect Answers:", 0xffffff, 1, 93);
     LCD.WriteAt(Player1->wAnswers, 93, 90);
 
     smallFont.display("Average Time:", 0xffffff, 2, 113);
@@ -500,10 +500,10 @@ void statsMenu(class Stats *Player1, class Stats *Player2){
 
     //lists player 2's stats
     LCD.WriteAt("Player 2:", 166, 50);
-    smallFont.display("Correct Answer:", 0xffffff, 168, 73);
+    smallFont.display("Correct Answers:", 0xffffff, 168, 73);
     LCD.WriteAt(Player2->cAnswers, 259, 70);
 
-    smallFont.display("Incorrect Answer:", 0xffffff, 167, 93);
+    smallFont.display("Incorrect Answers:", 0xffffff, 167, 93);
     LCD.WriteAt(Player2->wAnswers, 259, 90);
 
     smallFont.display("Average Time:", 0xffffff, 168, 113);
